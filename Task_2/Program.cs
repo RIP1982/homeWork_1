f2d43be101d5830247823d641f7e9d28d1f15321
RIP1982 <perovsky@list.ru>
2022-06-07 22:49:33 +0400

    I do it!

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -2,3 +2,20 @@
 // a = 5; b = 7 -> max = 7
 // a = 2; b = 10 -> max = 10
 // a = -9; b = -3 -> max = -3
+Console.Write("Введите число A ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA == numberB)
+{
+    Console.WriteLine("Число А " + numberA + " равно числу В " + numberB + "!");
+}
+else if(numberA > numberB)
+{
+    Console.Write("Число A = " + numberA + " больше числа B = " + numberB + "!"); 
+}
+else
+{
+   Console.Write("Число B = " + numberB + " больше числа A = " + numberA + "!"); 
+}
