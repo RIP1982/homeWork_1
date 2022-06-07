3e762c6f19b128420971bddd08cd646d07ece9a4
RIP1982 <perovsky@list.ru>
2022-06-07 23:10:54 +0400

    Find max

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2 3 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.Write("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число С: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+
+Console.Write("Максимальное число: " + max);
